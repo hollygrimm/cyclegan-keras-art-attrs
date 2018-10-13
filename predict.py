689d93be063b79262f753aba1b2f6b417ab0f7c5
Holly Grimm <holly.grimm@gmail.com>
2018-10-13 11:21:31 -0600

    predict on both, set A, or set B; config values for train/predict

--- predict.py
+++ predict.py
@@ -3,7 +3,7 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 import imageio
-from utils.utils import get_args, process_config, create_dirs
+from utils.utils import get_args, process_config
 from data_loader.data_loader import DataLoader
 from models.cyclegan_attr_model import CycleGANAttrModel
 from trainers.data_generator import DataGenerator
@@ -13,67 +13,42 @@ from trainers.data_generator import DataGenerator
 def main():
     # get json configuration filepath from the run argument
     # process the json configuration file
-    try:
-        args = get_args()
-        # TODO: Error if args.config doesn't exist
-        config, log_dir, checkpoint_dir = process_config(args.config)
-    except:
-        print('missing or invalid arguments')
-        print('Unexpected error:', sys.exc_info()[0])
-
-    # create the experiment directories
-    create_dirs([log_dir, checkpoint_dir])
+    args = get_args()
+    config = process_config(args.config)
 
     print('Create the data generator')
     data_loader = DataLoader(config)
 
     print('Create the model')
-    model = CycleGANAttrModel(config)
-    model.build_predict_model()
+    model = CycleGANAttrModel(config, is_train=False)
+    predict_set = config['predict_set']  # either a, b, both
+    model.build_predict_model(predict_set)
     print('model ready loading data now')
 
     os.makedirs('images/%s' % config['dataset_name'], exist_ok=True)
-    r, c = 2, 3
 
-    testA_datagen = DataGenerator(img_filenames=data_loader.get_testA_data(), batch_size=1, target_size=(config['predict_img_size_x'], config['predict_img_size_y']))
-    testB_datagen = DataGenerator(img_filenames=data_loader.get_testB_data(), batch_size=1, target_size=(config['predict_img_size_x'], config['predict_img_size_y']))
+    # TODO: Iterate through entire test datasets and write results to folder
+    if predict_set=='both' or predict_set=='a':
+        testA_datagen = DataGenerator(img_filenames=data_loader.get_testA_data(), batch_size=1, target_size=(config['predict_img_size_x'], config['predict_img_size_y']))
+        testA_generator = iter(testA_datagen)
+        imgs_A = next(testA_generator)
+        fake_B = model.predict_g_AB.predict(imgs_A)
+        imageio.imwrite("images/%s/a_transl.png" % (config['dataset_name']), ((fake_B[0]+1)*127.5).astype(np.uint8))
 
-    testA_generator = iter(testA_datagen)
-    testB_generator = iter(testB_datagen)
+    if predict_set=='both':
+        reconstr_A = model.predict_g_BA.predict(fake_B)
+        imageio.imwrite("images/%s/a_recon.png" % (config['dataset_name']), ((reconstr_A[0]+1)*127.5).astype(np.uint8))
 
-    imgs_A = next(testA_generator)
-    imgs_B = next(testB_generator)
+    if predict_set=='both' or predict_set=='b':
+        testB_datagen = DataGenerator(img_filenames=data_loader.get_testB_data(), batch_size=1, target_size=(config['predict_img_size_x'], config['predict_img_size_y']))
+        testB_generator = iter(testB_datagen)
+        imgs_B = next(testB_generator)    
+        fake_A = model.predict_g_BA.predict(imgs_B)
+        imageio.imwrite("images/%s/b_transl.png" % (config['dataset_name']), ((fake_A[0]+1)*127.5).astype(np.uint8))
 
-    # Translate images to the other domain
-    fake_B = model.predict_g_AB.predict(imgs_A)
-    fake_A = model.predict_g_BA.predict(imgs_B)
-
-    # Translate back to original domain
-    reconstr_A = model.predict_g_BA.predict(fake_B)
-    reconstr_B = model.predict_g_AB.predict(fake_A)
-
-    gen_imgs = np.concatenate([imgs_A, fake_B, reconstr_A, imgs_B, fake_A, reconstr_B])
-
-    # Rescale images 0 - 1
-    gen_imgs = 0.5 * gen_imgs + 0.5
-
-    # Plot images
-    titles = ['Original', 'Translated', 'Reconstructed']
-    fig, axs = plt.subplots(r, c)
-    cnt = 0
-    for i in range(r):
-        for j in range(c):
-            axs[i,j].imshow(gen_imgs[cnt])
-            axs[i, j].set_title(titles[j])
-            axs[i,j].axis('off')
-            cnt += 1
-    fig.savefig("images/%s/figure.png" % (config['dataset_name']))
-    plt.close()
-
-    imageio.imwrite("images/%s/a_transl.png" % (config['dataset_name']), ((fake_B[0]+1)*127.5).astype(np.uint8))
-    imageio.imwrite("images/%s/b_transl.png" % (config['dataset_name']), ((fake_A[0]+1)*127.5).astype(np.uint8))
-    imageio.imwrite("images/%s/a_recon.png" % (config['dataset_name']), ((reconstr_A[0]+1)*127.5).astype(np.uint8))
-    imageio.imwrite("images/%s/b_recon.png" % (config['dataset_name']), ((reconstr_B[0]+1)*127.5).astype(np.uint8))
+    if predict_set=='both':
+        reconstr_B = model.predict_g_AB.predict(fake_A)
+        imageio.imwrite("images/%s/b_recon.png" % (config['dataset_name']), ((reconstr_B[0]+1)*127.5).astype(np.uint8))
 
 if __name__ == '__main__':
     main()
